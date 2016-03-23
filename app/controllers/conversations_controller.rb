da4a895b236830c48c52f5b62dc3c09ffb15884a
F <fwrizalman@gmail.com>
2016-03-23 20:28:01 +0800

    Add mark as read

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,8 +2,24 @@ class ConversationsController < ApplicationController
   def index
     @conversations = current_user.mailbox.conversations
   end
-  
+
+  def inbox
+    @conversations = current_user.mailbox.inbox
+    render action: :index
+  end
+
+  def sent
+    @conversations = current_user.mailbox.sentbox
+    render action: :index
+  end
+
+  def trash
+    @conversations = current_user.mailbox.trash
+    render action: :index
+  end
+
   def new
+    @conversation = Mailboxer::Conversation.new
     @recipients = User.all - [current_user]
   end
 
@@ -15,5 +31,7 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = current_user.mailbox.conversations.find(params[:id])
+    @conversation.mark_as_read(current_user)
+    @message = Mailboxer::Message.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
 
   resources :conversations do
     resources :messages
+
+    collection do
+      get :inbox
+      get :all, action: :index
+      get :sent
+      get :trash
+    end
   end
 end
